a61ad121c42c93541549cee5e91d3ec5b6ef9b5b
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2017-12-16 16:10:24 +0000

    .new, .save, .delete_all working for vendors and tags - tested via seeds.rb

---
+++ models/tag.rb
@@ -0,0 +1,50 @@
+require_relative('../db/sql_runner.rb')
+
+class Tag
+
+  attr_reader(:id, :name)
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+  end
+
+  def save()
+    sql = "INSERT INTO tags
+    (name)
+    VALUES($1)
+    RETURNING *"
+
+    values = [@name]
+    tag_data = SqlRunner.run(sql, values)
+    @id = tag_data.first()['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM tags"
+    values = []
+    result = SqlRunner.run(sql, values)
+    tags = Tag.map_items(result)
+    return tags
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM tags WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values)
+    house = Tag.new(result.first)
+    return tag
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM tags"
+    values =[]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.map_items(tag_data)
+    result = tag_data.map {|tag| Tag.new(tag)}
+    return result
+  end
+
+end
---
+++ models/vendor.rb
@@ -0,0 +1,50 @@
+require_relative('../db/sql_runner.rb')
+
+class Vendor
+
+  attr_reader(:id, :name)
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+  end
+
+  def save()
+    sql = "INSERT INTO vendors
+    (name)
+    VALUES($1)
+    RETURNING *"
+
+    values = [@name]
+    vendor_data = SqlRunner.run(sql, values)
+    @id = vendor_data.first()['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM vendors"
+    values = []
+    result = SqlRunner.run(sql, values)
+    vendors = Vendor.map_items(result)
+    return vendors
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM vendors WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values)
+    house = Vendor.new(result.first)
+    return vendor
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM vendors"
+    values =[]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.map_items(vendor_data)
+    result = vendor_data.map {|vendor| Vendor.new(vendor)}
+    return result
+  end
+
+end
