dfcee5a699a65da4e28353d9bcecfefa49a1b8f1
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2017-12-16 17:30:14 +0000

    .new, .save, delete_all (transaction.rb)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 require_relative('../models/transaction')
 require_relative('../models/vendor')
 require_relative('../models/tag')
+require( 'pry-byebug' )
 
+Transaction.delete_all()
 Vendor.delete_all()
 Tag.delete_all()
-# Transaction.delete_all()
 
 vendor1 = Vendor.new({
   "name" => "Tesco"
@@ -58,13 +59,63 @@ clothes_tag.save()
 gift_tag.save()
 transport_tag.save()
 
-# transaction1 = Transaction.new({
-#   "vendor_id" => vendor1.id,
-#   "tag_id" => food_tag.id,
-#   "value" => 1000,
-#   "transaction_date" => 'December 12 2017'
-#   })
-#
-#
-#
-# transaction1.save()
+transaction1 = Transaction.new({
+  "vendor_id" => vendor1.id,
+  "tag_id" => food_tag.id,
+  "value" => 1000,
+  "transaction_date" => 'December 12 2017'
+  })
+
+transaction1.save()
+
+transaction2 = Transaction.new({
+  "vendor_id" => vendor2.id,
+  "tag_id" => drinks_tag.id,
+  "value" => 700,
+  "transaction_date" => 'December 10 2017',
+  "comment" => "G & T"
+  })
+
+transaction2.save()
+
+transaction3 = Transaction.new({
+  "vendor_id" => vendor3.id,
+  "tag_id" => gift_tag.id,
+  "value" => 2499,
+  "transaction_date" => 'December 15 2017'
+  })
+
+transaction3.save()
+
+transaction4 = Transaction.new({
+  "vendor_id" => vendor5.id,
+  "tag_id" => clothes_tag.id,
+  "value" => 3499,
+  "transaction_date" => 'December 11 2017',
+  "comment" => "Dress"
+  })
+
+transaction4.save()
+
+transaction5 = Transaction.new({
+  "vendor_id" => vendor2.id,
+  "tag_id" => drinks_tag.id,
+  "value" => 310,
+  "transaction_date" => 'December 15 2017',
+  "comment" => "Beer"
+  })
+
+transaction5.save()
+
+transaction6 = Transaction.new({
+  "vendor_id" => vendor4.id,
+  "tag_id" => transport_tag.id,
+  "value" => 160,
+  "transaction_date" => 'December 14 2017',
+  "comment" => "Bus"
+  })
+
+transaction6.save()
+
+# binding.pry
+# nil
---
+++ models/transaction.rb
@@ -0,0 +1,56 @@
+require_relative('../db/sql_runner.rb')
+
+class Transaction
+
+  attr_reader(:id, :vendor_id, :tag_id, :value, :transaction_date, :comment)
+
+  #extension field on transactions table - month_id ?
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @vendor_id = options['vendor_id'].to_i
+    @tag_id = options['tag_id'].to_i
+    @value = options['value'].to_i
+    @transaction_date = options['transaction_date']
+    @comment = options['comment'] if options['comment'] || nil
+  end
+
+  def save()
+    sql = "INSERT INTO transactions
+    (vendor_id, tag_id, value, transaction_date, comment)
+    VALUES($1, $2, $3, $4, $5)
+    RETURNING *"
+
+    values = [@vendor_id, @tag_id, @value, @transaction_date, @comment]
+    transaction_data = SqlRunner.run(sql, values)
+    @id = transaction_data.first()['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM transactions"
+    values = []
+    result = SqlRunner.run(sql, values)
+    transactions = Transaction.map_items(result)
+    return transactions
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM transactions WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values)
+    house = Transaction.new(result.first)
+    return transaction
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM transactions"
+    values =[]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.map_items(transaction_data)
+    result = transaction_data.map {|transaction| Transaction.new(transaction)}
+    return result
+  end
+
+end
