40a4dd5334826ba9ee401b5cae34d4f549e0c2ce
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2017-12-16 18:55:08 +0000

    Updated Money Tracker App.pages

---
+++ controllers/tags_controller.rb
@@ -0,0 +1,13 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative( '../models/tag.rb' )
+
+get '/tags' do
+  @tags = Tag.all()
+  erb ( :"tags/index" )
+end
+
+# get '/tags/:id' do
+#   @tag = Tag.find(params['id'].to_i)
+#   erb( :"tags/show" )
+# end
